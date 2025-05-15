88d7b05f9fe8d92471f9e129bdd00e345f84bccc
sneha <rajasinghamsneha@gmail.com>
2025-05-15 23:02:03 +0530

    Initialize project with basic server setup and package configuration

---
+++ index.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.method === 'GET' && req.url === '/') {
+        res.writeHead(200, { 'Content-Type': 'text/plain' });
+        res.end('Welcome to test server');
+    } else {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Not Found');
+    }
+});
+
+const PORT = 5000;
+server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
